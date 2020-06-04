5793880504800ead2efeed6a4f1995ef749d1af4
AElinevich <a.elinevich@tut.by>
2020-06-04 16:42:26 +0300

    create project with the first task

---
+++ script/script.js
@@ -0,0 +1,10 @@
+let money = '',
+    income = '',
+    addExpenses = '',
+    deposit = '',
+    mission = '',
+    period = '';
+
+alert('Жизнь без JavaScript не имеет смысла');
+
+console.log('Согласен');
