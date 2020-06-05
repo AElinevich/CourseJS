68b7458318d044d5b670c73ce3903dca1b59a5ee
AElinevich <a.elinevich@tut.by>
2020-06-05 16:48:47 +0300

    create new branch lesson02  and done task2

--- script/script.js
+++ script/script.js
@@ -1,10 +1,30 @@
-let money = '',
-    income = '',
-    addExpenses = '',
-    deposit = '',
-    mission = '',
-    period = '';
+let money = 3000;
+let income = 'Разработка сайтов';
+let addExpenses = 'Аренда, коммуналка, питание, шоппинг';
+let deposit = true;
+let mission = 100000;
+let period = 12;
+let budgetDay = money / 30;
 
-alert('Жизнь без JavaScript не имеет смысла');
+// alert('Жизнь без JavaScript не имеет смысла');
 
-console.log('Согласен');
+// console.log('Согласен');
+
+// task 2
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' долларов');
+
+console.log(addExpenses.toLowerCase().split());
+
+console.log(budgetDay);
+
+
+
+
+
