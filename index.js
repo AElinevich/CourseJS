6cd25312d269d365d4a368d1a27b83fa391e51c4
AElinevich <58183312+AElinevich@users.noreply.github.com>
2020-06-16 16:08:10 +0300

    done lesson09

---
+++ index.js
@@ -0,0 +1,34 @@
+// a
+let startBtn = document.getElementById('start');
+console.log(startBtn);
+// b
+let BtnIncomeAdd = document.getElementsByTagName('button')[0];
+let BtnExpensesAdd = document.getElementsByTagName('button')[1];
+console.log(BtnIncomeAdd);
+console.log(BtnExpensesAdd);
+// c
+let depositCheck  = document.querySelector('#deposit-check');
+console.log(depositCheck);
+// d
+let additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+console.log(additionalIncomeItem);
+// e
+let budgetDayValue = document.querySelector('.budget_day-value');
+let expensesMonthValue = document.querySelector('.expenses_month-value');
+let additionalIncomeValue = document.querySelector('.additional_income-value');
+let additionalExpensesValue = document.querySelector('.additional_expenses-value');
+let incomePeriodValue = document.querySelector('.income_period-value');
+let targetMonthValue = document.querySelector('.target_month-value');
+console.log(budgetDayValue, expensesMonthValue, additionalIncomeValue, additionalExpensesValue, incomePeriodValue, targetMonthValue);
+
+// f
+let salaryAmount = document.querySelector('.salary-amount');
+let incomeTitle = document.querySelector('input.income-title');
+let incomeAmount = document.querySelector('.income-amount');
+let expensesTitle = document.querySelector('input.expenses-title');
+let expensesAmount = document.querySelector('.expenses-amount');
+let additionalExpensesItem = document.querySelector('.additional_expenses-item');
+let targetAmount = document.querySelector('.target-amount');
+let periodSelect = document.querySelector('.period-select');
+console.log(salaryAmount, incomeTitle, incomeAmount, expensesTitle, expensesAmount, additionalExpensesItem, targetAmount, periodSelect);
+
