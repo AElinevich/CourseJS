2d51002619f4d538f1cb86e4dcccf489592e9d10
AElinevich <a.elinevich@tut.by>
2020-06-25 10:53:02 +0300

    correct lesson15.2

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 class First  {
-    constructor () {}
-hello () {
-console.log('Привет я метод родителя!')
+    hello () {
+        console.log('Привет я метод родителя!')
 }
 }
-
 
 class Second extends First {
-    constructor(hello) {
-    super(hello);    
-    }
-hello (){
-    super.hello(console.log('Привет я наследуемый метод!'))
+    hello() {
+    super.hello();    
+    console.log('Привет я наследуемый метод!')
 }
 }
 
-const firstTwo = new First();
 
 const secondTwo = new Second();
 console.log(secondTwo.hello());
