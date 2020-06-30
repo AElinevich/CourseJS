c8a48cc124a4ede093e776d7f2a04b75e0c58567
AElinevich <a.elinevich@tut.by>
2020-06-30 21:55:27 +0300

    done with hardtask

--- script/script.js
+++ script/script.js
@@ -117,5 +117,17 @@ popupClose.addEventListener('click', () => {
 
 togglePopup()
 
+// плавный скролл по якорям
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+    anchor.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        const blockID = anchor.getAttribute('href')
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
 
+    })
+}
 });
